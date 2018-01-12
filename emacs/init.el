22f8d771c080fa8a3b90e5adfdf2a7aa0e378a94
Eli Heuer <eliheuer@gmail.com>
2018-01-11 23:45:26 -0500

    consolidated all .els into one main file

--- emacs/init.el
+++ emacs/init.el
@@ -1,68 +1,49 @@
-;; ---------------------------------------------------------
-;; Eli Heuer's Emacs Config
-;; ---------------------------------------------------------
+;; ------------------------------------------------------------------------------
+;; Eli Heuer's Emacs Config: last updated 01-11-2018.
+;; ------------------------------------------------------------------------------
 
-;; Remove basic start screen
-(setq inhibit-startup-message t)
+;; Theme ------------------------------------------------------------------------
 
-;; Remove X-mode tool icons
-(tool-bar-mode -1)
+;; Load theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'campfire t)
 
-;; Move windows with Shift+arrow key
-(windmove-default-keybindings)
 
-;; Start in full screen mode
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; Typography -------------------------------------------------------------------
 
+; Set global typeface
+;(add-to-list 'default-frame-alist '(font . "-14"))
+;(set-default-font "Input")
+(set-frame-font "Input Mono Compressed-14:antialias=1")
+
+; disable bold fonts
 (set-face-bold-p 'bold nil)
 
-;; Info here
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-        (package-refresh-contents)
-	(package-install 'use-package))
-
-;; Info here
-(use-package try
-	:ensure t)
-
-;; Info here
-(use-package which-key
-	:ensure t 
-	:config
-	(which-key-mode))
+;; Line numbers -----------------------------------------------------------------
 
 ;; Line numbers
 (global-linum-mode t)
+; horizontal line highlight
+(global-hl-line-mode 1)
+; show horizontal cursor position
+(setq column-number-mode t)
 
-;; Dracula Theme
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (dracula)))
- '(custom-safe-themes
-   (quote
-    ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
-;; Put backup files neatly away                                                 
+;; GTK --------------------------------------------------------------------------
+
+;; Remove GTK X-mode items:
+(tool-bar-mode -1)
+;; (menu-bar-mode -1)
+;; (scroll-bar-mode -1)
+
+
+;; System ----------------------------------------------------------------------
+
+;; Copy/Paste stuff, old code, look into removing:
+;;(setq x-select-enable-clipboard t)
+;;(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+;; Put backup files neatly away
 (let ((backup-dir "~/tmp/emacs/backups")
       (auto-saves-dir "~/tmp/emacs/auto-saves/"))
   (dolist (dir (list backup-dir auto-saves-dir))
@@ -74,16 +55,124 @@
         tramp-backup-directory-alist `((".*" . ,backup-dir))
         tramp-auto-save-directory auto-saves-dir))
 
-(setq backup-by-copying t    ; Don't delink hardlinks                           
-      delete-old-versions t  ; Clean up the backups                             
-      version-control t      ; Use version numbers on backups,                  
-      kept-new-versions 5    ; keep some new versions                           
-      kept-old-versions 2)   ; and some old ones, too    
+(setq backup-by-copying t    ; Don't delink hardlinks
+      delete-old-versions t  ; Clean up the backups
+      version-control t      ; Use version numbers on backups,
+      kept-new-versions 5    ; keep some new versions
+      kept-old-versions 2)   ; and some old ones, too
+
+;; increase font size for better readability
+(set-face-attribute 'default nil :height 140)
+
+;; makes killing/yanking interact with the clipboard
+(setq
+      x-select-enable-clipboard t
+
+      ;; I'm actually not sure what this does but it's recommended?
+      x-select-enable-primary t
+
+      ;; Save clipboard strings into kill ring before replacing them.
+      ;; When one selects something in another program to paste it into Emacs,
+      ;; but kills something in Emacs before actually pasting it,
+      ;; this selection is gone unless this variable is non-nil
+      save-interprogram-paste-before-kill t
+
+      ;; Shows all options when running apropos. For more info,
+      ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
+      apropos-do-all t
+
+      ;; Mouse yank commands yank at point instead of at click.
+      mouse-yank-at-point t)
 
 
+;; Neotree ----------------------------------------------------------------------
+
+;; launch neotree on start
+(add-hook 'after-init-hook #'neotree-toggle)
+
+
+;; Startup ----------------------------------------------------------------------
+
+;; Start with just one window
+(add-hook 'emacs-startup-hook
+          (lambda () (delete-other-windows)) t)
+
+
+;; Org-mode ---------------------------------------------------------------------
+
+;; Org mode agenda setup
+(setq org-agenda-files
+  '("~/Journal/2017-12.org"))
+;;    "~/path/to/another/file.org"
+;;    "~/path/to/org-directory/"))
+
+;; Org-mode font size fix
+(defun my/org-mode-hook ()
+  "Stop the org-level headers from increasing in size"
+  (dolist (face '(org-level-1
+                  org-level-2
+                  org-level-3
+                  org-level-4
+                  org-level-5))
+    (set-face-attribute face nil :weight 'semi-bold :height 1.0)))
+
+;; org-mode
+(add-hook 'org-mode-hook 'my/org-mode-hook)
+
+
+;; Define package repositories --------------------------------------------------
+
+(require 'package)
+(setq package-enable-at-startup nil)
+;(add-to-list 'package-archives
+;         '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;(add-to-list 'package-archives
+;         '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+         '("melpa" . "https://melpa.org/packages/"))
+;(add-to-list 'package-archives
+;         '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; Load and activate emacs packages. Do this first so that the
+;; packages are loaded before you start trying to modify them.
+;; This also sets the load path.
+(package-initialize)
+
+;; Download the ELPA archive description if needed.
+;; This informs Emacs about the latest versions of all packages, and
+;; makes them available for download.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; The packages you want installed. You can also install these
+;; manually with M-x package-install
+;; Add in your own as you wish:
+(defvar my-packages
+  '(;; makes handling lisp expressions much, much easier
+    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+    paredit
+
+    ;; colorful parenthesis matching
+    rainbow-delimiters
+
+    ;; git integration
+    magit))
+
+
+;; GUI -------------------------------------------------------------------------
+
+;; Move windows with Shift+arrow key
+(windmove-default-keybindings)
+
+;; Start in full screen mode
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+
+;; Hotkeys ---------------------------------------------------------------------
+
+;; Duplicates the current line or region ARG times.
 (defun duplicate-current-line-or-region (arg)
-  "Duplicates the current line or region ARG times.
-If there's no region, the current line will be duplicated. However, if
+"If there's no region, the current line will be duplicated. However, if
 there's a region, all lines that region covers will be duplicated."
   (interactive "p")
   (let (beg end (origin (point)))
@@ -103,3 +192,140 @@ there's a region, all lines that region covers will be duplicated."
 
 (global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
 
+
+;; Vendor -----------------------------------------------------------------------
+
+;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
+;; to load them.
+;;
+;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
+;; then you can add the following code to this file:
+;;
+;; (require 'yaml-mode)
+;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+;; 
+;; Adding this code will make Emacs enter yaml mode whenever you open
+;; a .yml file
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+
+;; Navigation ------------------------------------------------------------------
+
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Uniquify.html
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+;; Turn on recent file mode so that you can more easily switch to
+;; recently edited files when you first start emacs
+(setq recentf-save-file (concat user-emacs-directory ".recentf"))
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 40)
+
+;; Shows a list of buffers
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Enhances M-x to allow easier execution of commands. Provides
+;; a filterable list of possible commands in the minibuffer
+;; http://www.emacswiki.org/emacs/Smex
+(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+
+
+;; Editing ---------------------------------------------------------------------
+
+;; Key binding to use "hippie expand" for text autocompletion
+;; http://www.emacswiki.org/emacs/HippieExpand
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+;; Lisp-friendly hippie expand
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
+;; Highlights matching parenthesis
+(show-paren-mode 1)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Interactive search key bindings. By default, C-s runs
+;; isearch-forward, so this swaps the bindings.
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+;; Don't use hard tabs
+(setq-default indent-tabs-mode nil)
+
+
+;; Emacs can automatically create backup files. This tells Emacs to
+;; put all backups in ~/.emacs.d/backups. More info:
+;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+(setq auto-save-default nil)
+
+
+;; comments
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+;; yay rainbows!
+(global-rainbow-delimiters-mode t)
+
+;; use 4 spaces for tabs
+(defun die-tabs ()
+  (interactive)
+  (set-variable 'tab-width 4)
+  (mark-whole-buffer)
+  (untabify (region-beginning) (region-end))
+  (keyboard-quit))
+
+;; (setq electric-indent-mode nil)
+
+
+;; Lisp ------------------------------------------------------------------------
+
+;; Automatically load paredit when editing a lisp file
+;; More at http://www.emacswiki.org/emacs/ParEdit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;; eldoc-mode shows documentation in the minibuffer when writing code
+;; http://www.emacswiki.org/emacs/ElDoc
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
+
+;; Misc ------------------------------------------------------------------------
+
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; No need for ~ files when editing
+(setq create-lockfiles nil)
+
+;; Go straight to scratch buffer on startup
+(setq inhibit-startup-message t)
+
+
+;; End of init.el.
